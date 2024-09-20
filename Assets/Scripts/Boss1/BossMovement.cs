8b23d0b0c06121bd8402477c1371d7c89b34fbf1
Mert Kavilcioğlu <mertkavilci@gmail.com>
2024-09-20 15:42:45 +0300

    Added Boss Movement

---
+++ Assets/Scripts/Boss1/BossMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float bossSpeed = 7.5f;
+    public GameObject player;
+    public float playerSpeed = 7.5f;
+    public float distanceThreshold = 10f;
+    public float raycastDistance = 10f;
+
+    private PlayerMovement playerMovement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(bossSpeed, rb.velocity.y);
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer < distanceThreshold)
+        {
+            playerSpeed = Mathf.Max(7.5f, bossSpeed + (distanceThreshold - distanceToPlayer));
+            playerMovement.SetMoveSpeed(playerSpeed);
+        }
+        else
+        {
+            playerSpeed = bossSpeed;
+            playerMovement.SetMoveSpeed(playerSpeed);
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, raycastDistance);
+        Debug.DrawRay(transform.position, Vector2.right * raycastDistance, Color.red);
+    }
+}
