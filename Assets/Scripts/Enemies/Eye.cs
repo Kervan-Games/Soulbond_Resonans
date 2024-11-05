15d2210ab51c5627bc29f5fdc1345270a6f8d72a
Mert Kavilcioğlu <mertkavilci@gmail.com>
2024-11-05 22:59:48 +0300

    Level Design

--- Assets/Scripts/Enemies/Eye.cs
+++ Assets/Scripts/Enemies/Eye.cs
@@ -32,7 +32,7 @@ public class Eye : MonoBehaviour
             {
                 animator.SetBool("isClosed", isClosed);
             }
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(2f);
         }
     }
 
